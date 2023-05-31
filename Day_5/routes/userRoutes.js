79e125dcee169f04b1a5c709d14f13343f76c273
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-05-31 13:29:04 +0300

    created auth controller

--- Day_5/routes/userRoutes.js
+++ Day_5/routes/userRoutes.js
@@ -1,69 +1,37 @@
 const express = require("express");
 
 const router = express.Router();
-const fs = require("fs");
-const AppError = require("../utils/AppError");
-const path = require("path");
-const User = require("../models/user");
-const { findByIdAndUpdate } = require("../models/to-do");
 
-router.use("/", (req, res, next) => {
-  console.log("request happend at ", Date.now());
-  req.test = "mohamed";
-  next();
-});
+
+const { signUp, getUsers, getUserById, updateUserById, deleteUserById } = require("../controllers/authenticationController");
+
+// middleware
+// router.use("/", (req, res, next) => {
+//   console.log("request happend at ", Date.now());
+//   req.test = "mohamed";
+//   next();
+// });
+
 // restful api (users) : crud operations // create, read , update, delete
 // users
 
 // get users
-router.get("/", async (req, res, next) => {
-  const users = await User.find();
-  res.send(users);
-});
-// get user by id
-router.get("/:id", async (req, res,next) => {
-  const { id } = req.params;
-  const user = await User.findById(id);
-  if(!user) return next(new AppError('user not found',404))
-  res.send(user);
-});
-// create users
-router.post("/", async (req, res,next) => {
-  // try {
-  const { email, password } = req.body;
-  if(!email || !password){
-    // const error = new Error('email and password are required');
-    // err.status = 'error'
-    // error.statusCode = 400;
-    // return next(error)
-    next(new AppError('email and password required',400));
-  }
-  // const userCreated = await User.create({email,password});
-  const userCreated = new User({ email, password });
-  await userCreated.save();
-  userCreated.password = undefined;
-  res.send(userCreated);
-  // } catch (err) {
-  //   console.log('err', err)
-  //   next(err);
-  // }
-});
+router.get("/", getUsers);
+// Get User by ID
+router.get("/:id", getUserById);
+// Register User
+router.post("/", signUp);
+
 // update users
 // put : replace the old document with the new document
 // patch: only modifies certain properties inside the document
-router.patch("/:id", async(req, res) => {
-  const { id } = req.params;
-  const { email, password } = req.body;
-  await User.findByIdAndUpdate(id,{email, password});
-});
+router.put("/:id", updateUserById);
+router.patch("/:id", updateUserById);
 // delete users
-router.delete("/:id", async(req, res) => {
-  const { id } = req.params;
-  await User.findByIdAndDelete(id);
-});
+router.delete("/:id", deleteUserById);
 
-router.use((req, res, next) => {
-  console.log(req.data);
-});
+// router.use((req, res, next) => {
+//   console.log(req.data);
+// });
 
 module.exports = router;
