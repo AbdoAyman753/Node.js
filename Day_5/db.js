1cbc9a2f33f31a6ef91b1ccc8a85ea23f21944dc
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-08 00:35:29 +0300

    used .env parameters

--- Day_5/db.js
+++ Day_5/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/mearn')
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log('connected to db'))
 .catch((err)=>{
 	console.log(err)
--- Day_5/index.js
+++ Day_5/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('express-async-errors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const path = require('path');
 const morgan = require('morgan');
 const { get } = require('http');
